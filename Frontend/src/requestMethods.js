83aff3b93bf73d6db20befe7ece8d078801d7860
“Dinesh <ddinesh@citchennai.net>
2024-12-27 17:03:45 +0530

    Testing Frontend Connection with Backend

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // Dynamically set the BASE_URL based on the environment
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL =  "https://backend-repo-wndn.onrender.com/api/v1";
+;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
