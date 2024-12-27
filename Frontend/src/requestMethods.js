c8cff79a2f9491cbf3d23cb2e824a19aa9dfabd2
“Dinesh <ddinesh@citchennai.net>
2024-12-27 14:05:36 +0530

    Modified changes

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1";
+// Dynamically set the BASE_URL based on the environment
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://backend-repo-wndn.onrender.com/api/v1" // Deployed backend URL
+    : "http://localhost:8000/api/v1"; // Localhost URL
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
